5bcdbc53bbae308bd015c9dc41efae57bf6d726b
dizzy <dzchoi@gmail.com>
2019-08-29 05:36:23 -0400

    Changing test.pl

--- source/test.pl
+++ source/test.pl
@@ -4,19 +4,29 @@ use File::Spec::Functions qw(rel2abs);
 use File::Basename;
 
 our $INCLUDE = dirname(rel2abs $0);
-our $TEST_CPP = "test.cpp";
 our $A_OUT = "a.out";
 
 chdir "/tmp";
 
-sub build_test_cpp($) {
-    open FILE, '>', $TEST_CPP or die "$TEST_CPP: $!\n";
-    print FILE $_[0];
-    close FILE;
-    system "clang++", "-I$INCLUDE", $TEST_CPP, "-lpthread" and die $!;
+sub build_a_out($) {
+    system "echo '$_[0]' |clang++ -I$INCLUDE -lpthread -xc++ -"
+        and die "clang++ error: $?";
 }
 
-build_test_cpp <<EOF;
+sub start_a_out($) {
+    my $parent_pid = $$;
+
+    defined(my $child_pid = fork) or die "fork error: $!";
+    unless ( $child_pid ) {
+        exec "$_[0]";
+        kill 2, $parent_pid;  # SIGINT
+        die "$A_OUT error: $!";
+    }
+
+    return $child_pid;
+}
+
+build_a_out <<EOF;
 #include "process.hpp"
 #include "fdstream.hpp"
 #include <iostream>
@@ -28,7 +38,11 @@ int main()
 }
 EOF
 
-system "$A_OUT &" and die $!;
+my $pid = start_a_out "./a.out";
+print "pid = $pid";
 
 my $ps = `ps`;
 print $ps;
+
+waitpid $pid, 0;
+
